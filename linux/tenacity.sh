66637819f3b13398fe2206bacc969743d7f0e0a5
haruo <haruo86@live.jp>
2021-10-05 23:45:24 +0900

    bug fix for locale

--- linux/tenacity.sh
+++ linux/tenacity.sh
@@ -2,6 +2,9 @@
 
 lib="${0%/*}/lib/tenacity"
 share="${0%/*}/share/tenacity"
+LD_LIBRARY_PATH="${0%/*}/lib"
+AUDACITY_PATH="${0%/*}/share/locale"
+
 
 export LD_LIBRARY_PATH="${lib}:${LD_LIBRARY_PATH}"
 export AUDACITY_MODULES_PATH="${AUDACITY_MODULES_PATH}:${lib}/modules"
